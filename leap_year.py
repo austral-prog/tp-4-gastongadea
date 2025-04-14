1713e87a8ac83fc8569dfdca49171abe42cacde9
Gastón Gadea <53276272+gastongadea@users.noreply.github.com>
2025-04-14 10:52:12 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,9 @@
 def leap_year():
-    print("TO DO")
+    anio=int(input("Ingrese un año: "))
+    if anio/4==anio//4:
+        if anio/100==anio//100 and anio/400==anio//400:
+            print(f"El año {anio} es bisiesto")
+        else:
+            print(f"El año {anio} no es bisiesto")
+    else:
+        print(f"El año {anio} no es bisiesto")
