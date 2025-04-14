dbfaec9b951df6596b10c31d49880cdb7b39d462
Gastón Gadea <53276272+gastongadea@users.noreply.github.com>
2025-04-14 09:33:43 -0300

    Update line.py

--- line.py
+++ line.py
@@ -1,2 +1,18 @@
 def line():
-    print("TO DO")
+    a=float(input("Ingrese el coeficiente A: "))
+    b=float(input("Ingrese el coeficiente B: "))
+    x1=float(input("Ingrese el coeficiente X1: "))
+    x2=float(input("Ingrese el coeficiente X2: "))
+    print(f"El coeficiente A de su ecuación de la recta es: {a}")
+    print(f"El coeficiente B de su ecuación de la recta es: {b}")
+    print(f"El coeficiente X1 de su ecuación de la recta es: {x1}")
+    print(f"El coeficiente X2 de su ecuación de la recta es: {x2}\n")
+    y1=x1*a-b
+    y2=x2*a-b
+    print("Para la siguiente ecuación:")
+    print(f"\tY = {a}X + {b}\n")
+    print("Dados los siguientes puntos:")
+    dist=((x2-x1)**2+(y2-y1)**2)**(1/2)
+    print(f"\tP1 {x1}, {y1}")
+    print(f"\tP2 {x1}, {y2}")
+    print(f"\nLa distancia entre ellos es: {dist}")
